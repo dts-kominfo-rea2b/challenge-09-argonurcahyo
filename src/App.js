e19e0a7236c8c96701e6df26df55dc38ccec75ad
Argo Nurcahyo <argonurcahyo25@gmail.com>
2022-06-19 20:36:39 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
+import { Contact } from './components/Contact';
+import { Header } from './components/Header';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
